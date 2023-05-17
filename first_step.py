a7f0c31d9173c7a3e1e85747d73505df23a2c367
Alexandra Solovyova <alexandrasolovjova@mail.ru>
2023-05-17 04:42:41 +0300

    learn a numpy

--- first_step.py
+++ first_step.py
@@ -1,3 +1,12 @@
 import numpy as np
+from scipy import sparse
+
 x = np.array([[1, 2, 3], [4, 5, 6]])
 print("x:\n{}".format(x))
+
+eye = np.eye(4)
+print("Массив NumPy:\n{}".format(eye))
+
+sparse_matrix = sparse.csr_matrix(eye)
+print("\nРазреженна матрица SciPy в формате CSR:\n{}".format(sparse_matrix))
+
